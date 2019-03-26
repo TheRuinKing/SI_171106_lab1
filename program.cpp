9acb439b4ea742f94444c6412d72e0d8e5aec6fc
TRKing <antonio.avramovic@gmail.com>
2019-03-26 16:51:24 +0100

    Initial commit

--- program.cpp
+++ program.cpp
@@ -3,14 +3,13 @@
 using namespace std;
 
 void printMyName () {
-	cout<<"Antonio Avramovic"<<endl;
+
 }
 
 void printMyIndex () {
-	cout<<"171106"<<endl;
+
 }
 
 int main () {
-	printMyName();
-	printMyIndex();
+
 }
